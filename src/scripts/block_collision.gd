0650a4d9a8e739fd9f0c1611354a56ec56d00236
Hugo Rivera <hrivera@nmt.edu>
2015-02-26 21:06:39 -0700

    sped up fly-away motion

--- src/scripts/block_collision.gd
+++ src/scripts/block_collision.gd
@@ -20,17 +20,17 @@ func activate(ev, click_pos, click_normal):
 	tween_count += 1
 	var tween_node = get_node(tween_name)
 	
-	if actions.find(ACTION_SCALE):
+	if actions.find(ACTION_SCALE) > -1:
 		var s = 0.1
 		tween_node.interpolate_method( self, "set_scale", \
 			self.get_scale(), Vector3(s, s, s), \
 			1, Tween.TRANS_BOUNCE, Tween.EASE_OUT )
 			
-	if actions.find(ACTION_FLY_AWAY):
+	if actions.find(ACTION_FLY_AWAY) > -1:
 		# Efficient use of Tween?
 		tween_node.interpolate_method( self, "set_translation", \
 			self.get_translation(), self.get_translation().normalized() * far_away_corner, \
-			1, Tween.TRANS_CIRC, Tween.EASE_IN_OUT )
+			1, Tween.TRANS_QUAD, Tween.EASE_OUT )
 			
 	tween_node.start()
 
