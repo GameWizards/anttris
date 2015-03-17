59b3fb4aa87ed10445e8c6658c7e3190ce8ebfdd
Skyler <skymanzanares@hotomail.com>
2015-03-16 18:21:14 -0600

    Created the menu scene, and put in a temp background.

---
+++ src/menu.gd
@@ -0,0 +1,13 @@
+
+extends TextureFrame
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	get_node("StartButton").connect("pressed", self, "_start_pressed")
+	pass
+
+func _start_pressed():
+	get_node("/node/global").goto_scene("res://puzzle.scn")
