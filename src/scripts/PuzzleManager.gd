f025107d665cfab5993260345a19ef0cabb5ccb4
hucal <hrivera@nmt.edu>
2015-04-13 01:31:55 -0600

    paired blocks working. changed to camelCase.

--- src/scripts/PuzzleManager.gd
+++ src/scripts/PuzzleManager.gd
@@ -1,21 +1,16 @@
-var PUZZLE_5x5		= 5
-var PUZZLE_7x7		= 7
+const PUZZLE_5x5		= 5
+const PUZZLE_7x7		= 7
+const blockColors = ["Blue", "Orange", "Red", "Yellow"]
 
-var BLOCK_RED		= 0
-var BLOCK_GREEN		= 1
-var BLOCK_BLUE		= 2
-var BLOCK_YELLOW	= 3
-var BLOCK_ORANGE	= 4
-var BLOCK_PURPLE	= 5
+# Preload paired blocks
+const pairedBlock = preload("Blocks/PairedBlock.gd")
+const flyBlock = preload("Blocks/FlyawayTestBlock.gd")
+const blockScn = preload( "res://blocks/block.scn" )
 
-# Stores information about a single block for a puzzle.
-class Block:
-	var blockType = 0
-	var blockPos = Vector3( 0, 0, 0 )
 
 # Stores a puzzle in a convenient class.
 class Puzzle:
-	var puzzleSize
+	var puzzleType
 	var blocks = []
 
 # Holds all of the steps needed to solve a puzzle.
@@ -25,7 +20,7 @@ class PuzzleSteps:
 # Randomly shuffle an array.
 func shuffleArray( arr ):
 	for i in range( arr.size() - 1, 1, -1 ):
-		print( "SWAP" )
+		# works great! print( "SWAP" )
 		var swapVal = randi() % (i + 1)
 		var temp = arr[swapVal]
 		arr[swapVal] = arr[i]
@@ -33,8 +28,9 @@ func shuffleArray( arr ):
 
 # Generates a solveable puzzle.
 func generatePuzzle( type ):
+	var blockID = 0
 	var puzzle = Puzzle.new()
-	puzzle.puzzleSize = type
+	puzzle.puzzleType = type
 	
 	var middle = type / 2
 	
@@ -43,18 +39,30 @@ func generatePuzzle( type ):
 		for y in range( type ):
 			for z in range( type ):
 				if !(x == middle && y == middle && z == middle):
-					var tb = Block.new()
+					var tb = PickledBlock.new()
+					tb.name = "block" + str(blockID)
+					blockID += 1
 					tb.blockPos = Vector3( x, y, z )
 					puzzle.blocks.append( tb )
 	
+	# TODO, proposed algorithm:
+	# make blocks, pairs are adjacent
+	# shuffle board, half the blocks pick a "nearby" block to swap places with
+	# nearby = same layer, somewhere accessible to the user
+	
 	# Randomize the order of the blocks.
-	self.shuffleArray( puzzle.blocks )
+	#self.shuffleArray( puzzle.blocks )
 	
 	# Assign block types in pairs.
 	for i in range( 0, puzzle.blocks.size(), 2 ):
-		var randBlock = randi() % 6
-		puzzle.blocks[i].blockType = randBlock
-		puzzle.blocks[i+1].blockType = randBlock
+		var randColor = blockColors[randi() % blockColors.size()]
+		puzzle.blocks[i].setBlockClass("PairedBlock") \
+			.setPairName(puzzle.blocks[i+1].name) \
+			.setTextureName(randColor)
+			
+		puzzle.blocks[i+1].setBlockClass("PairedBlock") \
+			.setPairName(puzzle.blocks[i].name) \
+			.setTextureName(randColor)
 	
 	return puzzle
 	
@@ -72,3 +80,39 @@ func solvePuzzleSteps( puzzle ):
 	# SOLVER
 	
 	return puzzleSteps
+	
+
+class PickledBlock:
+	var name
+	var blockClass
+	var pairName
+	var textureName
+	var blockPos
+	
+	func setName(n):
+		name = n
+		return self
+		
+	func setPairName(n):
+		pairName = n
+		return self
+		
+	func setBlockClass(c):
+		blockClass = c
+		return self
+		
+	func setTextureName(t):
+		textureName = t
+		return self
+
+	func toNode(gen):
+		# instantiate a block scene, assign the appropriate script to it
+		var n = blockScn.instance()
+		n.set_script(load("res://scripts/Blocks/" + blockClass + ".gd"))
+		
+		# configure block node
+		n.setName(name)
+
+		if blockClass == "PairedBlock":
+			n.setPairName(pairName).setTexture(textureName)
+		return n
