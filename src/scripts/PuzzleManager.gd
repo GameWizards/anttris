24a3b9d72a2de80445abc993abdcd7fac047fd11
hucal <hrivera@nmt.edu>
2015-04-14 20:30:49 -0600

    testing gdscript.vim, no real changes

--- src/scripts/PuzzleManager.gd
+++ src/scripts/PuzzleManager.gd
@@ -8,6 +8,7 @@ const flyBlock = preload("Blocks/FlyawayTestBlock.gd")
 const blockScn = preload( "res://blocks/block.scn" )
 
 
+
 # Stores a puzzle in a convenient class.
 class Puzzle:
 	var puzzleType
@@ -16,7 +17,7 @@ class Puzzle:
 # Holds all of the steps needed to solve a puzzle.
 class PuzzleSteps:
 	var solveable
-	
+
 # Randomly shuffle an array.
 func shuffleArray( arr ):
 	for i in range( arr.size() - 1, 1, -1 ):
@@ -31,16 +32,16 @@ func generatePuzzle( type ):
 	var blockID = 0
 	var puzzle = Puzzle.new()
 	puzzle.puzzleType = type
-	
+
 	var middle = type / 2
-	
+
 	# create all possible positions
 	var shape = []
 	for x in range( type ):
 		for y in range( type ):
 			for z in range( type ):
 				shape.append(Vector3(x,y,z))
-	
+
 	# assign blocks to positions
 	var prevBlock = null
 	var even = false
@@ -53,10 +54,10 @@ func generatePuzzle( type ):
 		var b = PickledBlock.new()
 		b.blockPos = pos
 		b.name = "block" + str(blockID)
-		
+
 		blockID += 1
 		puzzle.blocks.append( b )
-			
+
 		if (x == y and y == z):
 			b.setBlockClass("LaserBlock")
 		else:
@@ -65,51 +66,51 @@ func generatePuzzle( type ):
 				b.setBlockClass("PairedBlock") \
 					.setPairName(prevBlock.name) \
 					.setTextureName(randColor)
-	
+
 				prevBlock.setBlockClass("PairedBlock") \
 					.setPairName(b.name) \
 					.setTextureName(randColor)
 			even = not even
 			prevBlock = b
-				
 
-	
+
+
 	# TODO, proposed algorithm:
 	# make blocks, pairs are adjacent
 	# shuffle board, half the blocks pick a "nearby" block to swap places with
 	# nearby = same layer, somewhere accessible to the user
-	
+
 	# Randomize the order of the blocks.
 	#self.shuffleArray( puzzle.blocks )
-	
+
 	# Assign block types in pairs.
 #	for i in range( 0, puzzle.blocks.size(), 2 ):
 #		var randColor = blockColors[randi() % blockColors.size()]
 #		puzzle.blocks[i].setBlockClass("PairedBlock") \
 #			.setPairName(puzzle.blocks[i+1].name) \
 #			.setTextureName(randColor)
-#			
+#
 #		puzzle.blocks[i+1].setBlockClass("PairedBlock") \
 #			.setPairName(puzzle.blocks[i].name) \
 #			.setTextureName(randColor)
-	
+
 	return puzzle
-	
+
 # Determines if a puzzle is solveable.
 func solvePuzzle( puzzle ):
 	# Simply use the SolvePuzzleSteps function and return the solveable part.
 	var ps = solvePuzzleSteps()
 	return ps.solveable
-	
+
 # Determines if a puzzle is solveable and returns the steps needed to solve it.
 func solvePuzzleSteps( puzzle ):
 	var puzzleSteps = PuzzleSteps.new()
 	puzzleSteps.solveable = true
-	
+
 	# SOLVER
-	
+
 	return puzzleSteps
-	
+
 
 class PickledBlock:
 	var name
@@ -117,23 +118,23 @@ class PickledBlock:
 	var pairName
 	var textureName
 	var blockPos
-	
+
 	func setName(n):
 		name = n
 		return self
-		
+
 	func setPairName(n):
 		pairName = n
 		return self
-		
+
 	func setBlockClass(c):
 		blockClass = c
 		return self
-		
+
 	func setTextureName(t):
 		textureName = t
 		return self
-		
+
 	func toString():
 		return str(name) + ": " + str(blockClass)
 
@@ -141,7 +142,7 @@ class PickledBlock:
 		# instantiate a block scene, assign the appropriate script to it
 		var n = blockScn.instance()
 		n.set_script(load("res://scripts/Blocks/" + blockClass + ".gd"))
-		
+
 		# configure block node
 		n.setName(name).setTexture()
 
