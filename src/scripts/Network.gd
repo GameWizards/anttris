a0c9d7dedc4ec5f50a80cbf7d8854083fa7fea45
Skyler <skymanzanares@hotmail.com>
2015-04-16 20:23:26 -0600

    Began to work on specific processing of server data

--- src/scripts/Network.gd
+++ src/scripts/Network.gd
@@ -1,6 +1,13 @@
 
 extends Node
 
+const REMOTE_FINISH = 0
+const REMOTE_START = 1
+const REMOTE_QUIT = 2
+const REMOTE_SCORE = 3
+const REMOTE_BLOCK = 4
+const REMOTE_MSG = 5
+
 var is_network
 var is_host
 var server
@@ -53,9 +60,12 @@ func _process(delta):
 				return
 			
 			#check if we have any data
-			if get_available_packet_count():
-				var data_array = stream.get_var()
-				
+			if stream.get_available_packet_count():
+				#have to be careful about more than 1 packet per frame
+				for i in range(stream.get_available_packet_count()):
+					var data_array = stream.get_var()
+					process_server_data(data_array)
+					
 	else:
 		#client processing stuff
 		if !is_network:
@@ -70,4 +80,29 @@ func _process(delta):
 				#stop running process loop, cause we have no connection
 			
 		else:
-			#connecton established and confirmed. Do regular data processioin
+			#connecton established and confirmed. Do regular data processing
+			
+
+
+
+
+
+
+
+func process_server_data(data_array):
+	#have an array of data. First element should be identifying int
+	var ID = data_array[0]
+	
+	#no swithc statement. I'm crying right now while i type this. My fingers are bleeding
+	if ID == REMOTE_START:
+		#do start something or something whut
+	elif ID == REMOTE_FINISH:
+		#again, do ending stuff
+	elif ID == REMOTE_QUIT:
+		#omg those jerks!
+	elif ID == REMOTE_SCORE:
+		#what was their score?
+	elif ID == REMOTE_BLOCK:
+		#get their block ifnormation!
+	elif ID == REMOTE_MSG:
+		#sent some sort of message?
