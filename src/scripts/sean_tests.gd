2b96e9fc4e3ec411d28823273b91ffa9a37aacff
hucal <hrivera@nmt.edu>
2015-04-18 16:33:17 -0600

    instantiated pickles!

--- src/scripts/sean_tests.gd
+++ src/scripts/sean_tests.gd
@@ -1,10 +1,14 @@
 extends "res://scripts/gut.gd".Test
 
+var puzzleManScript = load("res://scripts/PuzzleManager.gd")
+
 func setup():
 	gut.p("ran setup", 2)
-	
+
 func teardown():
 	gut.p("ran teardown")
-	
+
 func test_assert_eq_number_is_equal():
+	var pMan = puzzleManScript.new()
+	var pickle = pMan.PickledBlock.new()
 	gut.assert_eq('asf', 'asdf', "Should pass")
