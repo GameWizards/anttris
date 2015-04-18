df77921c3642df922f9dc52346d97e9ede70507c
Sean <sturner@nmt.edu>
2015-04-18 16:05:44 -0600

    testing123

---
+++ src/scripts/sean_tests.gd
@@ -0,0 +1,10 @@
+extends "res://scripts/gut.gd".TestCube
+
+func setup():
+	gut.p("ran setup", 2)
+	
+func teardown():
+	gut.p("ran teardown")
+	
+func test_assert_eq_number_is_equal():
+	gut.assert_eq('asf', 'asdf', "Should pass")
