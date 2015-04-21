e21c0c39cfc4ecfa6c3fe951c6d3e4318e47bb68
hucal <hrivera@nmt.edu>
2015-04-21 14:57:21 -0600

    close the program once tests are done

--- src/scripts/TestDriver.gd
+++ src/scripts/TestDriver.gd
@@ -7,3 +7,8 @@ func _ready():
 	tester.set_should_print_to_console(true)
 	tester.add_script('res://scripts/sean_tests.gd')
 	tester.test_scripts()
+
+	# close on test end
+	print("\n\tResources in use:")
+	OS.print_resources_in_use(true)
+	OS.get_main_loop().quit()
