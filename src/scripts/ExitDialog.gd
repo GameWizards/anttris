3df9b8726d81c7d4950d6113df6aaef3557cfabd
Sean <sturner@nmt.edu>
2015-04-27 00:49:55 -0600

    broke code fix? maybe?

--- src/scripts/ExitDialog.gd
+++ src/scripts/ExitDialog.gd
@@ -2,15 +2,15 @@
 extends Node
 
 var SceneLoader = preload( "res://scripts/SceneSwitcher.gd" )
-var Dialog = ConfirmationDialog.new()
+var Dialog
 
 func _on_ok_button_pressed():
 	goto_scene("res://menus.scn")
 
-func show_dialog():
-	Dialog.show()
-
 func _ready():
+	Dialog = ConfirmationDialog.new()
 	Dialog.set_script(self)
 	Dialog.set_text("Really quit? All unsaved progress will be lost!")
 	Dialog.get_ok().connect("pressed", self, "_on_ok_button_pressed")
+	print("showing dialog...")
+	Dialog.show()
