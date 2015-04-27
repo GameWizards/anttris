0281b08fc2a3451598194c6b30380a6c6e3b5ed8
Sean <sturner@nmt.edu>
2015-04-27 01:20:25 -0600

    Move to my own function, rather than override
    _ready()

--- src/scripts/ExitDialog.gd
+++ src/scripts/ExitDialog.gd
@@ -7,13 +7,21 @@ var Dialog
 func _on_ok_button_pressed():
 	goto_scene("res://menus.scn")
 
-
-
-func _ready():
+func show_dialog():
 	print("Got ready call in exit script")
 	Dialog = ConfirmationDialog.new()
 	#Dialog.set_script(self)
+	Dialog.popup_centered()
 	Dialog.set_text("Really quit? All unsaved progress will be lost!")
 	Dialog.get_ok().connect("pressed", self, "_on_ok_button_pressed")
 	print("showing dialog...")
 	Dialog.show()
+
+#func _ready():
+#	print("Got ready call in exit script")
+#	Dialog = ConfirmationDialog.new()
+#	#Dialog.set_script(self)
+#	Dialog.set_text("Really quit? All unsaved progress will be lost!")
+#	Dialog.get_ok().connect("pressed", self, "_on_ok_button_pressed")
+#	print("showing dialog...")
+#	Dialog.show()
