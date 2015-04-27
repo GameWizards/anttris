af457e77a0f89db41da526eed234ccb764f6e46c
Sean <sturner@nmt.edu>
2015-04-27 01:48:09 -0600

    Cleanup and stuff

--- src/scripts/ExitDialog.gd
+++ src/scripts/ExitDialog.gd
@@ -5,23 +5,13 @@ var SceneLoader = preload( "res://scripts/SceneSwitcher.gd" )
 var Dialog
 
 func _on_ok_button_pressed():
-	goto_scene("res://menus.scn")
+	SceneLoader.goto_scene("res://menus.scn")
 
 func show_dialog():
 	print("Got ready call in exit script")
 	Dialog = ConfirmationDialog.new()
-	#Dialog.set_script(self)
-	Dialog.popup_centered()
+	Dialog.set_title("Are you sure?")
 	Dialog.set_text("Really quit? All unsaved progress will be lost!")
 	Dialog.get_ok().connect("pressed", self, "_on_ok_button_pressed")
 	print("showing dialog...")
-	Dialog.show()
-
-#func _ready():
-#	print("Got ready call in exit script")
-#	Dialog = ConfirmationDialog.new()
-#	#Dialog.set_script(self)
-#	Dialog.set_text("Really quit? All unsaved progress will be lost!")
-#	Dialog.get_ok().connect("pressed", self, "_on_ok_button_pressed")
-#	print("showing dialog...")
-#	Dialog.show()
+	Dialog.popup_centered()
