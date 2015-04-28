f64fcd01645c13be05bf41753e35e2a6068bb7e6
hucal <hrivera@nmt.edu>
2015-04-28 12:11:23 -0600

    renamed prevBlock to prevBlokcs. saving and loading work. file dialog initilization made static.

--- src/scripts/Editor.gd
+++ src/scripts/Editor.gd
@@ -2,11 +2,14 @@ extends Spatial
 
 var puzzle
 var puzzleMan
+var DataMan = preload("res://scripts/DataManager.gd")
 
 var gridMan
-var prevBlock = [] # keeps track of prevous color for pairs by layer
+var prevBlocks = [] # keeps track of prevous color for pairs by layer
 var blockColors = preload("res://scripts/PuzzleManager.gd").blockColors
 var id
+
+var saveDir = OS.get_data_dir() + "/PuzzleSaves"
 
 var fd
 var gui = [
@@ -57,22 +60,25 @@ func addBlock(pos):
 	var layer = puzzleMan.calcBlockLayerVec(pos)
 	id += 1
 
+	if layer == 0 and not gui[class_ix][1].value == "GOAL":
+		return
+
 	# expand prevblocks index
-	while prevBlock.size() <= layer:
+	while prevBlocks.size() <= layer:
 		var d = {}
 		for k in blockColors:
 			d[k] = null
-		prevBlock.append(d)
+		prevBlocks.append(d)
 
-	var pb = prevBlock[layer][curColor]
+	var pb = prevBlocks[layer][curColor]
 
 	if pb != null:
 		b.setPairName(pb.name)
 		gridMan.get_node(pb.toNode().name).setPairName(b.name)
-		prevBlock[layer][curColor] = null
+		prevBlocks[layer][curColor] = null
 	else:
 		# TODO SUPPORT GLYPHS HERE, SET PAIRWISE GLYPH
-		prevBlock[layer][curColor] = b
+		prevBlocks[layer][curColor] = b
 
 	gui[0][1].set_text(getPrevBlockErrors())
 	var block = gridMan.addPickledBlock(b)
@@ -85,11 +91,11 @@ func getPrevBlockErrors():
 	# hahaha, so bad
 	var missing = "STATUS: "
 	# check every layer
-	for l in range(prevBlock.size()):
+	for l in range(prevBlocks.size()):
 		# check every color
-		for k in prevBlock[l].keys():
+		for k in prevBlocks[l].keys():
 			var missed = false
-			if prevBlock[l][k] != null:
+			if prevBlocks[l][k] != null:
 				# one message about the current layer
 				if not missed:
 					missing += " L" + str(l) + " "
@@ -124,7 +130,7 @@ func _ready():
 	set_process_input(true)
 
 	var theme = preload("res://themes/MainTheme.thm")
-	initLoadSaveDialog(self)
+	fd = initLoadSaveDialog(self, get_tree(), saveDir)
 
 	var y = 0
 	for control in gui:
@@ -149,58 +155,70 @@ func _ready():
 			if e extends Button:
 				e.set_text(control[0])
 			if control[0] == 'save_pzl' :
-				e.connect('pressed', self, 'showSaveDialog')
+				e.connect('pressed', self, 'showSaveDialog', [fd, self])
 			if control[0] == 'load_pzl' :
-				e.connect('pressed', self, 'showLoadDialog')
+				e.connect('pressed', self, 'showLoadDialog', [fd, self])
 			if control[0] == 'status':
 				control[1].set_text('STATUS: NOMINAL')
 			add_child(e)
 
 
 func puzzleSave():
-	print("SAVING TO ", fd.get_current_file() )
+	var f = fd.get_current_path()
+	if f == null or f == "":
+		return
+	print("SAVING TO ", f)
+	DataMan.savePuzzle( f, gridMan.puzzle )
 
 func puzzleLoad():
-	print("LOADING FROM ", fd.get_current_file() )
+	var f = fd.get_current_path()
+	if f == null or f == "":
+		return
+	print("LOADING FROM ", f)
+	prevBlocks = []
+	gridMan.set_puzzle(DataMan.loadPuzzle( f ))
 
-func showLoadDialog():
-	showSaveDialog(true)
+static func showLoadDialog(fileD, parent):
+	if fileD.is_connected("confirmed", parent, "puzzleSave"):
+		fileD.disconnect("confirmed", parent, "puzzleSave")
+	fileD.connect("confirmed", parent, "puzzleLoad")
+	fileD.set_mode(FileDialog.MODE_OPEN_FILE)
 
-func showSaveDialog(loadInstead=false):
-	if loadInstead:
-		if fd.is_connected("confirmed", self, "puzzleSave"):
-			fd.disconnect("confirmed", self, "puzzleSave")
-		fd.connect("confirmed", self, "puzzleLoad")
-		fd.set_mode(FileDialog.MODE_OPEN_FILE)
-	else:
-		if fd.is_connected("confirmed", self, "puzzleLoad"):
-			fd.disconnect("confirmed", self, "puzzleLoad")
-		fd.connect("confirmed", self, "puzzleSave")
-		fd.set_mode(FileDialog.MODE_SAVE_FILE)
+	fileD.popup_centered()
 
-	fd.popup_centered()
+static func showSaveDialog(fileD, parent):
+	if fileD.is_connected("confirmed", parent, "puzzleLoad"):
+		fileD.disconnect("confirmed", parent, "puzzleLoad")
+	fileD.connect("confirmed", parent, "puzzleSave")
+	fileD.set_mode(FileDialog.MODE_SAVE_FILE)
 
-func initLoadSaveDialog(parent):
+	fileD.popup_centered()
+
+static func initLoadSaveDialog(parent, tree, saveDir):
 	# setup text in the file dialog
-	parent.fd = load("res://fileDialog.scn").instance()
-	parent.fd.set_title("Select Puzzle Filename")
-	parent.fd.set_access(FileDialog.ACCESS_USERDATA)
-	parent.fd.set_current_dir("PuzzleSaves")
-	parent.fd.add_filter("*.pzl ; Anttris Puzzle")
+	var fileDialog = load("res://fileDialog.scn").instance()
+	fileDialog.set_title("Select Puzzle Filename")
+	fileDialog.set_access(FileDialog.ACCESS_FILESYSTEM)
+	fileDialog.add_filter("*.pzl ; Anttris Puzzle")
+
+	Directory.new().make_dir_recursive(saveDir)
+	fileDialog.set_current_dir(saveDir)
+	print("Saves in ", saveDir)
 
 	# MainTheme leaks on bottom
 	var dialogTheme = Theme.new()
 	dialogTheme.copy_default_theme()
-	parent.fd.set_theme(dialogTheme)
+	fileDialog.set_theme(dialogTheme)
 
 	# work even if game is paused
-	parent.fd.set_pause_mode(PAUSE_MODE_PROCESS)
+	fileDialog.set_pause_mode(PAUSE_MODE_PROCESS)
 
 	# unpause if user cancels
-	parent.fd.connect("popup_hide", get_tree(), "set_pause", [false])
-	parent.fd.connect("hide", get_tree(), "set_pause", [false])
-	parent.fd.connect("about_to_show", get_tree(), "set_pause", [true])
-	parent.fd.hide()
+	fileDialog.connect("popup_hide", tree, "set_pause", [false])
+	fileDialog.connect("hide", tree, "set_pause", [false])
+	fileDialog.connect("about_to_show", tree, "set_pause", [true])
+	fileDialog.hide()
 
-	parent.add_child(fd)
-	fd.set_owner(parent)
+	parent.add_child(fileDialog)
+	fileDialog.set_owner(parent)
+	return fileDialog
